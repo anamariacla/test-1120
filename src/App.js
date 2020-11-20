0cd9171209409f73bbc848e62cfd950a80475920
Oprea Claudia-Anamaria <claudiaoprea16@gmail.com>
2020-11-20 19:32:38 +0200

    App test test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
+
+class Counter extends Component {
+  render() {
+    const { children, name, start } = this.props;
+
+    return (
+      <div>
+        <div>Hello, {name}</div>
+        {children}
+        My counter, start: {start}
+      </div>
+    );
+  }
+}
 
 export default function App() {
   return (
@@ -7,6 +21,13 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>test</p>
+      <Counter name="Claudia" start={0}>
+        <span>Test: </span>
+      </Counter>
+      <br />
+      <Counter name="Anamaria" start={3}>
+        <span>Test: </span>
+      </Counter>
     </div>
   );
 }
